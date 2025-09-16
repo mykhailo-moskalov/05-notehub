0c1b19c763e70d51c952cdd26610909f406c28b0
mishokkartoshki <misha@moskalov.com>
2025-09-16 14:20:01 +0200

    init

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+// import Pagination from "../Pagination/Pagination";
+// import SearchBox from "../SearchBox/SearchBox";
+import css from "./App.module.css";
+
+export default function App() {
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        {/* <SearchBox /> */}
+        {/* <Pagination /> */}
+        {/* Кнопка створення нотатки */}
+      </header>
+    </div>
+  );
+}
---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,5 @@
+import css from "./Pagination.module.css";
+
+export default function Pagination() {
+  return <></>;
+}
