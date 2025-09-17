93c523e8ff4c0e0a57a742f2a48caa7942950323
Mykhailo Moskalov <misha@moskalov.com>
2025-09-17 21:39:34 +0200

    fix NoteList

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,12 +1,25 @@
 import type { Note } from "../../types/note";
 import css from "./NoteList.module.css";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteNote } from "../../services/noteService";
+import toast from "react-hot-toast";
 
 interface NoteListProps {
   notes: Note[];
-  onDelete: (id: string) => void;
 }
 
-export default function NoteList({ notes, onDelete }: NoteListProps) {
+export default function NoteList({ notes }: NoteListProps) {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onSuccess: () => {
+      toast.success("Note deleted!");
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+    onError: () => toast.error("Failed to delete note.");
+  });
+
   return (
     <ul className={css.list}>
       {notes.map((n) => (
@@ -18,7 +31,7 @@ export default function NoteList({ notes, onDelete }: NoteListProps) {
             <button
               className={css.button}
               onClick={() => {
-                if (n.id) onDelete(n.id);
+                if (n.id) mutate(n.id);
               }}
             >
               Delete
